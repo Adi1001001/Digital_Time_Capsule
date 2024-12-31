d0f56a327c5bf0e2c13acea4ddaf9700b542bd3a
Adi <134138142+Adi1001001@users.noreply.github.com>
2024-12-31 17:16:05 +1100

    Added the time feature in digital_time_capsule.py
    when the user clicks on the button 'create memory' the time is stored into several different variables. For the reminders and features like that I created a total_days variable so that it will be easier to compute different reminders and add them to the queue, but I have still kept the normal full date in different variables like final_month and final_day.
    
    I have also experimented with a new colour theme that could work, but that is still a work in progress and I want to finish the main code logic without worrying too much about the aesthetics.

--- digital_time_capsule.py
+++ digital_time_capsule.py
@@ -2,25 +2,100 @@ import tkinter as tk
 import ttkbootstrap as ttk
 from PIL import ImageFont
 from tkinter import font
+from datetime import datetime
 
 # standards
 dark_grey = '#1b1c18'
 white_text = '#eef2df'
+# colour set
+sky_blue = '#90AFC5'
+deep_rust = '#763626'
+dark_charcoal = '#2A3132'
 Camelia_Path = "C:\\Users\\Adi\\Coding\\zfonts_RNS_Camelia.ttf"
 Camelia = ImageFont.truetype(Camelia_Path, size = 16)
 
 # window configuration
 window = ttk.Window(themename = 'journal')
 window.title('Digital Time Capsule')
-window.geometry('600x400')
+window.geometry('1000x800')
 window.resizable(True, True) # allowing the window to be resizable
-window.configure(bg = dark_grey)
+window.configure(bg = dark_charcoal)
 
 # main stuff
 print('the window is on right now')
-title_text = ttk.Label(master = window, text = 'The Digital Time Capsule', font = ("Times New Roman", 24, "bold"), background = dark_grey, foreground = white_text)
+title_text = ttk.Label(master = window, text = 'The Digital Time Capsule', font = ("Times New Roman", 24, "bold"), background = dark_charcoal, foreground = sky_blue)
 title_text.pack(pady = 25)
 
+memory_input = tk.Text(master = window, background = sky_blue)
+memory_input.pack()
+
+def create_new_memory():
+    date = str(datetime.today())
+    date, date_time = date.split(' ')
+    date_hour, date_minute = date_time.split(':')[:2]
+    final_hour = int(date_hour)
+    date_minute = int(date_minute)
+    if date_minute >= 30:
+        final_hour += 1
+    final_year, final_month, final_day = date.split('-')
+    final_year = int(final_year)
+    final_month = int(final_month)
+    final_day = int(final_day)
+    total_days = 0
+    #amount of days
+    if final_month > 1:
+        total_days += 31
+        if final_month > 2:
+            total_days += 28
+            if final_month > 2:
+                if final_month > 3:
+                    total_days += 31
+                    if final_month > 4:
+                        total_days += 30
+                        if final_month > 5:
+                            total_days += 31
+                            if final_month > 6:
+                                total_days += 30
+                                if final_month > 7:
+                                    total_days += 31
+                                    if final_month > 8:
+                                        total_days += 31
+                                        if final_month > 9:
+                                            total_days += 30
+                                            if final_month > 10:
+                                                total_days += 31
+                                                if final_month > 11:
+                                                    total_days += 30
+                                                    total_days += final_day
+                                                else:
+                                                    total_days += final_day
+                                            else:
+                                                total_days += final_day
+                                        else:
+                                            total_days += final_day
+                                    else:
+                                        total_days += final_day
+                                else:
+                                    total_days += final_day
+                            else:
+                                total_days += final_day
+                        else:
+                            total_days += final_day
+                    else:
+                        total_days += final_day
+                else:
+                    total_days += final_day
+            else:
+                total_days += final_day
+        else:
+            total_days += final_day
+    else:
+        total_days += final_day
+    print(total_days)
+
+new_memory_button = tk.Button(window, text = "Create a new memory", command = create_new_memory, font = ("Times New Roman", 12), foreground = deep_rust)
+new_memory_button.pack(pady = 20)
+    
 window.mainloop()
 
 # after window is closed
